5fbbb7897cbd836ae85dfb712ce847ad295d4005
gp <plumpmath@gmail.com>
2023-04-28 06:33:05 +0900

    u

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 let
   mvn2nix = import (fetchTarball "https://github.com/fzakaria/mvn2nix/archive/master.tar.gz") {};
   mavenRepository = mvn2nix.buildMavenRepositoryFromLockFile { file = ./mvn2nix-lock.json; };
-  inherit (pkgs) lib stdenv maven makeWrapper jdk11_headless;
+  inherit (pkgs) lib stdenv maven makeWrapper jdk17_headless;
   inherit (stdenv) mkDerivation;
 in
 mkDerivation rec {
@@ -12,7 +12,7 @@ mkDerivation rec {
   version = "0.0.1-SNAPSHOT";
   name = "${pname}-${version}";
   src = lib.cleanSource ./.;
-  nativeBuildInputs = [ jdk11_headless maven makeWrapper ];
+  nativeBuildInputs = [ jdk17_headless maven makeWrapper ];
   buildPhase = ''
     mvn package spring-boot:repackage -X --offline -Dmaven.repo.local=${mavenRepository}
   '';
@@ -20,7 +20,7 @@ mkDerivation rec {
     mkdir -p $out/bin
     ln -s ${mavenRepository} $out/lib
     cp target/${name}.jar $out/
-    makeWrapper ${jdk11_headless}/bin/java $out/bin/${pname} \
+    makeWrapper ${jdk17_headless}/bin/java $out/bin/${pname} \
     --add-flags "-jar $out/${name}.jar"
   '';
 }
