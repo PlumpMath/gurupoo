957caedcd75bb5d4b314c29af5d47cc02953dc10
gp <plumpmath@gmail.com>
2023-04-28 06:01:21 +0900

    init

---
+++ src/main/java/plumpmath/gurupoo/HelloController.java
@@ -0,0 +1,7 @@
+package plumpmath.gurupoo;
+
+public class HelloController {
+    public String hello(String name){
+        return "Hello " + name;
+    }
+}
---
+++ src/test/java/plumpmath/gurupoo/GurupooApplicationTests.java
@@ -0,0 +1,13 @@
+package plumpmath.gurupoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GurupooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
