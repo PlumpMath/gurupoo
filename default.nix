1a24c3e8b727a50674fe2697c1556c2245ae06ae
gp <plumpmath@gmail.com>
2023-04-28 06:25:02 +0900

    u

--- default.nix
+++ default.nix
@@ -1,39 +1,26 @@
+
 { pkgs ? import <nixpkgs> {} }:
+
 let
-  mvn2nix = import
-    (fetchTarball "https://github.com/fzakaria/mvn2nix/archive/master.tar.gz")
-    { };
-  mavenRepository =
-   mvn2nix.buildMavenRepositoryFromLockFile { file = ./mvn2nix-lock.json; };
-inherit (pkgs) lib stdenv jdk11_headless maven makeWrapper;
-inherit (stdenv) mkDerivation;
-in mkDerivation rec {
+  mvn2nix = import (fetchTarball "https://github.com/fzakaria/mvn2nix/archive/master.tar.gz") {};
+  mavenRepository = mvn2nix.buildMavenRepositoryFromLockFile { file = ./mvn2nix-lock.json; };
+  inherit (pkgs) lib stdenv maven makeWrapper graalvm17-ce;
+  inherit (stdenv) mkDerivation;
+in
+mkDerivation rec {
   pname = "gurupoo";
   version = "0.0.1-SNAPSHOT";
   name = "${pname}-${version}";
   src = lib.cleanSource ./.;
-
-  nativeBuildInputs = [ jdk11_headless maven makeWrapper ];
+  nativeBuildInputs = [ graalvm17-ce maven makeWrapper ];
   buildPhase = ''
-    echo "Building with maven repository ${mavenRepository}"
-    mvn package spring-boot:repackage -X --offline -Dmaven.repo.local=${mavenRepository}
-  '';
-
+    mvn clean install spring-boot:repackage -X -Dmaven.repo.local=${mavenRepository}
+  ''; # --offline
   installPhase = ''
-    # create the bin directory
     mkdir -p $out/bin
-
-    # create a symbolic link for the lib directory
     ln -s ${mavenRepository} $out/lib
-
-    # copy out the JAR
-    # Maven already setup the classpath to use m2 repository layout
-    # with the prefix of lib/
     cp target/${name}.jar $out/
-
-    # create a wrapper that will automatically set the classpath
-    # this should be the paths from the dependency derivation
-    makeWrapper ${jdk11_headless}/bin/java $out/bin/${pname} \
-          --add-flags "-jar $out/${name}.jar"
+    makeWrapper ${graalvm17-ce}/bin/java $out/bin/${pname} \
+    --add-flags "-jar $out/${name}.jar"
   '';
 }
