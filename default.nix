0af24530fc71032eefa97c7c697967034f0d0a74
gp <plumpmath@gmail.com>
2023-04-28 19:42:40 +0900

    u

--- default.nix
+++ default.nix
@@ -3,25 +3,25 @@
 with pkgs;
 let
   mvn2nix = import (fetchTarball "https://github.com/fzakaria/mvn2nix/archive/master.tar.gz") {};
-  # mavenRepository = mvn2nix.buildMavenRepositoryFromLockFile { file = ./mvn2nix-lock.json; };
-  # repository = (buildMaven ./project-info.json).repo;
-  inherit (pkgs) lib stdenv maven makeWrapper jdk17_headless;
+  inherit (pkgs) lib stdenv maven makeWrapper;
   inherit (stdenv) mkDerivation;
+  JDK = graalvm17-ce;
 in
 mkDerivation rec {
   pname = "gurupoo";
   version = "0.0.1-SNAPSHOT";
   name = "${pname}-${version}";
   src = lib.cleanSource ./.;
-  nativeBuildInputs = [ jdk17_headless maven makeWrapper ];
+  buildInputs = [ maven ];
+  dontFixup = true;
+  nativeBuildInputs = [  JDK makeWrapper ];
   buildPhase = ''
-    mvn -Dmaven.repo.local=$out package spring-boot:repackage
+    mvn -Dmaven.repo.local=$out install spring-boot:repackage
   '';
-    # ln -s ${mavenRepository} $out/lib
   installPhase = ''
     mkdir -p $out/bin
     cp target/${name}.jar $out/
-    makeWrapper ${jdk17_headless}/bin/java $out/bin/${pname} \
+    makeWrapper ${JDK}/bin/java $out/bin/${pname} \
     --add-flags "-jar $out/${name}.jar"
   '';
 }
